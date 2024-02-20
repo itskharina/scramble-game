8e878e5fddef0979d6dab588d71e52b4296b75b1
itskharina <137315976+itskharina@users.noreply.github.com>
2024-02-20 23:21:37 +0000

    Added logic that checks if guess is correct

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ const scramble = document.querySelector('.scramble');
 const random = document.querySelector('.random');
 const reset = document.querySelector('.reset');
 const type = document.querySelector('.type');
-const box1 = document.querySelector('.box-1');
-const box2 = document.querySelector('.box-2');
-const box3 = document.querySelector('.box-3');
-const box4 = document.querySelector('.box-4');
-const box5 = document.querySelector('.box-5');
-const box6 = document.querySelector('.box-6');
+// const box1 = document.querySelector('#1');
+// const box2 = document.querySelector('#2');
+// const box3 = document.querySelector('#3');
+// const box4 = document.querySelector('#4');
+// const box5 = document.querySelector('#5');
+// const box6 = document.querySelector('#6');
 
-let guess = [];
+const guess = []; // stores what user types
+let word = '';
 
 async function fetchWord() {
   try {
@@ -23,17 +24,18 @@ async function fetchWord() {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const word = await response.json();
+    word = await response.json();
     console.log(word);
-    scrambled([word].join(''));
+    scrambled(word.join(''));
     return word;
   } catch (error) {
     console.error('An error occured:', error);
   }
 }
 
+//scrambles word
 function scrambled(str) {
-  arr = Array.from(str);
+  arr = str.split('');
   for (let i = 0; i < arr.length - 1; i++) {
     let j = Math.floor(Math.random() * arr.length);
     let temp = arr[i];
@@ -47,16 +49,43 @@ function scrambled(str) {
 
 fetchWord();
 
+// adds letters to ui when typed
 type.addEventListener('input', function (e) {
   const input = e.target;
-  guess.push(input.value);
+
+  if (input.value) {
+    guess.splice(input.dataset.index - 1, 1, input.value);
+  }
+
   const next = input.nextElementSibling;
   console.log(guess);
 
   if (next && input.value) {
     next.focus();
   }
+
+  if (input.nextElementSibling == null) {
+    checkGuess();
+  }
 });
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Backspace' || e.key === 'Del') {
+    const index = e.target.dataset.index - 1;
+    guess.splice(index, 1, '');
+  }
+});
+
+function checkGuess() {
+  let guessJoined = guess.join('');
+  console.log(guessJoined);
+
+  if (guessJoined == word) {
+    console.log('correct');
+  } else {
+    console.log('wrong');
+  }
+}
 
 random.addEventListener('click', function () {
   fetchWord();
